7477f244975bb551da46ff6625dfd425f2da60ff
SergStas <sergstas0@gmail.com>
2021-09-16 11:01:08 +0500

    block 2

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,19 +1,46 @@
 package com.example.demoweb.model;
 
-public class Post {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text, Integer likes) {
+    protected Post() {}
+
+    public Post(int id, String text, Integer likes, Date creationDate) {
+        this.id = (long) id;
         this.text = text;
         this.likes = likes;
+        this.creationDate = creationDate;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 
     public Integer getLikes() {
         return likes;
     }
 
-    public String getText() {
-        return text;
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,17 +3,27 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+    private List<Post> posts;
+
+    public PostService() {
+        this.posts = new ArrayList<>();
+        posts.add(new Post(0, "Post 5675671", 1, new Date(1345647)));
+        posts.add(new Post(1, "Post 5675671", 2, new Date(1573457)));
+        posts.add(new Post(2, "Post 5675671", 0, new Date(14357)));
+        posts.add(new Post(3, "Post 5675671", 4, new Date(34574561)));
+    }
+
     public List<Post> listAllPosts() {
-        return Arrays.asList(
-                new Post("United Russia", 340097),
-                new Post("LDPR", 404),
-                new Post("Apple", 12),
-                new Post("Other opposition", -2)
-        );
+        return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(posts.size(), text,1, new Date()));
     }
 }
